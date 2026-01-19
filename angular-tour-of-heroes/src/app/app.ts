ff05e5613594438f9aabc32b2ba64a82d67fa4ee
JustDaveHere-School <davidemaximiliano_muzzi@sluz.ch>
2026-01-19 11:21:39 +0100

    complete step 1

--- angular-tour-of-heroes/src/app/app.ts
+++ angular-tour-of-heroes/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroesComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('angular-tour-of-heroes');
+  title = 'Tour of Heroes';
 }
