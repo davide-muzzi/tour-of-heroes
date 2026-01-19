7fa45fc7fd0f7e052bc813179b68a2c1141721f5
JustDaveHere-School <davidemaximiliano_muzzi@sluz.ch>
2026-01-19 11:33:12 +0100

    complete step 2

--- angular-tour-of-heroes/src/app/heroes/heroes.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -10,8 +11,10 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './heroes.scss',
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
